48b3c2a22ba7bf77c9ca330fea863aeb730ace1f
jhonygiraldo <jhonygiraldoz@gmail.com>
2024-12-29 19:58:02 +0000

    Deploying to gh-pages from @ jhonygiraldo/jhonygiraldo.github.io@ebadf09d4370649dd7668fbf5cc5c464c1960c18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,14 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+    },{id: "news-our-paper-hygene-a-diffusion-based-hypergraph-generation-method-has-been-accepted-at-the-aaai-conference-on-artificial-intelligence-2025",
+          title: 'Our paper “HYGENE: A Diffusion-based Hypergraph Generation Method” has been accepted at the...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-our-paper-higher-order-gnns-meet-efficiency-sparse-sobolev-graph-neural-networks-was-accepted-in-ieee-transactions-on-signal-and-information-processing-over-networks",
+          title: 'Our paper “Higher-Order GNNs Meet Efficiency: Sparse Sobolev Graph Neural Networks” was accepted...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-ve-been-awarded-the-academic-grant-program-from-nvidia-for-my-project-bridging-the-gap-between-foundation-models-and-camera-trap-image-recognition",
+          title: 'I’ve been awarded the Academic Grant Program from NVIDIA for my project Bridging...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
