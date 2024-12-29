f546d89d37ce35b290f2ca3bd408d9bb01c53257
jhonygiraldo <jhonygiraldoz@gmail.com>
2024-12-29 21:07:57 +0000

    Deploying to gh-pages from @ jhonygiraldo/jhonygiraldo.github.io@03890bcb0d27d3b226f9442ee396884d12ee5267 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -444,6 +444,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("jhonyhgiraldo.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -451,18 +458,11 @@ ninja.data = [{
           window.open("mailto:%6A%68%6F%6E%79.%67%69%72%61%6C%64%6F@%74%65%6C%65%63%6F%6D-%70%61%72%69%73.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/jhonyhgiraldo", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -470,13 +470,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=iwzmGKwAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
